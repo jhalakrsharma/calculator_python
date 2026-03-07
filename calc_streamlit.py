9469dd7cb58d8d493b3c5311c252583571292e11
Jhalak Sharma <45706725+jhalakrsharma@users.noreply.github.com>
2026-03-07 20:12:42 +0100

    Update calc_streamlit.py
    deleted 'for me'

--- calc_streamlit.py
+++ calc_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 from calc_backend import add_number, subtract_number, divide_number, square_root
 
 st.set_page_config(page_title="Basic Calculator", page_icon="🧮")
-st.title("Basic Calculator for me")
+st.title("Basic Calculator")
 
 num1 = st.number_input("Enter first number", value=0.0)
 num2 = st.number_input("Enter second number", value=0.0)
@@ -16,5 +16,6 @@ if st.button("Calculate"):
         st.write(f"Division: {divide_number(num1, num2)}")
     except ValueError as e:
         st.write(f"Division: {e}")
+
 
     st.write(f"Square root of first number: {square_root(num1)}")
